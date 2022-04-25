b7fe78aa5cf62d93b6ca592fcb9ae91b3574871c
Luke Van Seters <lukevanseters@gmail.com>
2022-04-25 13:42:04 -0400

    Write as we go

--- main.py
+++ main.py
@@ -19,23 +19,23 @@ def is_stolen(asset_contract_address: str, token_id: int) -> str:
 
 
 def main():
-    stolen_token_ids = []
+    total_stolen = 0
 
-    for token_id in range(1, 10001):
-        message = "not stolen"
-
-        if is_stolen(BAYC_CONTRACT_ADDRESS, token_id):
-            message = "stolen"
-            stolen_token_ids.append(token_id)
-        
-        print(f"{token_id} - {message}")
-
-        # rate limit
-        sleep(0.1)
-
-    print(f"Total stolen: {len(stolen_token_ids)}")
     with open("stolen.txt", "w") as stolen_file:
-        stolen_file.write("\n".join(map(str, stolen_token_ids)))
+        for token_id in range(1, 10001):
+            message = "not stolen"
+
+            if is_stolen(BAYC_CONTRACT_ADDRESS, token_id):
+                message = "stolen"
+                stolen_file.write(f"{token_id}\n")
+                total_stolen += 1
+            
+            print(f"{token_id} - {message}")
+
+            # rate limit
+            sleep(0.1)
+
+    print(f"Total stolen: {total_stolen}")
 
 
 if __name__ == "__main__":
